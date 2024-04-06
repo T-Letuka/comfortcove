6a1db3f8b503ca88642818045bfbf5ef095379db
T-Letuka <tisetsoletuka@gmail.com>
2024-04-07 00:18:23 +0200

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password || username === " " || email === " ") {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 8);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json({ message: "Successful Signup" });
+  } catch (error) {
+    res.status(400).json({ message: "Akuhambanga kahle:something is wrong" });
+  }
+};
