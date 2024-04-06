a356aa94c3b4b135e6c1f20f356066b1cfdf6429
T-Letuka <tisetsoletuka@gmail.com>
2024-04-06 22:08:47 +0200

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Mongodb is connected perfectly");
+  })
+  .catch((err) => {
+    console.log("Something is wrong error");
+  });
 
 const app = express();
 
