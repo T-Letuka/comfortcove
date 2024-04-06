695274e244372bd699b443929ed4b22bcb0e3e77
T-Letuka <tisetsoletuka@gmail.com>
2024-04-06 10:09:24 +0200

    created pages and installed the setup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Posts from "./pages/Posts";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
